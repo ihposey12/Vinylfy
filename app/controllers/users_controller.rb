6cac424e55ee6583554ffca383bcfa8dbb012bf6
Hunter Posey <ihposey@crimson.ua.edu>
2021-01-04 21:12:42 -0800

    beginning view/controller/model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :username, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :records, through: :user_records
 
     has_many :reviews
+
+    validates :name, :username, :password, presence: true
+    validates :username, uniqueness: true
+    validates :password, length: {minimum: 7}
 end
